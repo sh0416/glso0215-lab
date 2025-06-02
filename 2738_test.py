fa900ad6e87e3449a34dccbd0407879b8747058c
wonjun jang <fshrnnn@gmail.com>
2025-06-02 11:05:53 +0900

    change solution file name and add testcase file

---
+++ 2738_test.py
@@ -0,0 +1,25 @@
+# example input test case 1
+3 3
+1 1 1
+2 2 2
+0 1 0
+3 3 3
+4 4 4
+5 5 100
+
+# example output test case 1
+4 4 4
+6 6 6
+5 6 100
+
+# example input test case 2
+2 2
+1 4
+1 5
+2 1
+3 3
+
+# example output test case 2
+3 5
+4 8
+
